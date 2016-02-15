98820ee6c72f66e48d5e1157bba432cca47d0324
benselme <benselme@gmail.com>
2016-02-15 14:27:56 -0500

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Mako',
-    version='0.3',
+    version='0.4',
     url='https://github.com/benselme/flask-mako',
     license='BSD',
     author='Beranger Enselme, Frank Murphy',
